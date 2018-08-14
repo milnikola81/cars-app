f858e283c4c63d43255633bcb1ce90092f913980
Nikola <milnikola81@gmail.com>
2018-08-14 18:27:57 +0200

    finished no.1

---
+++ src/components/AppCars.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+        <h1>App Cars Vue</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
--- src/main.js
+++ src/main.js
@@ -1,8 +1,23 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueRouter from 'vue-router'
+import AppCars from './components/AppCars.vue'
+
+Vue.use(VueRouter);
+
+const routes = [
+    {path: '/', redirect: '/cars'},
+    {path: '/cars', component: AppCars}
+    // {path: '/contacts/:id', component: Contacts, name: 'contact-details'}
+];
+
+const router = new VueRouter({
+  routes
+});
+
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App)
+  render: h => h(App), router
 }).$mount('#app')
